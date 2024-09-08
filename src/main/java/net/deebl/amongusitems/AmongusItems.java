21b123435e584ab3f2701dd7e9613d331af8f1b1
Howie bray <howiebray@gmail.com>
2024-09-08 19:42:22 +0100

    Initial commit

---
+++ src/main/java/net/deebl/amongusitems/AmongusItems.java
@@ -0,0 +1,15 @@
+package net.deebl.amongusitems;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AmongusItems implements ModInitializer {
+	public static final String MOD_ID = "amongus-items";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
